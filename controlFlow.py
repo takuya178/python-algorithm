f3c6de13c2085508bc20e62aaca03eb51445e510
takuya178 <takuya.u1005@gmail.com>
2023-05-18 18:49:41 +0900

    factorial

--- controlFlow.py
+++ controlFlow.py
@@ -1,12 +1,16 @@
-def redirectionUrl(language: str) -> str:
-    domain = 'www.example.org/'
-    if language == 'English':
-        return domain + 'en'
-    elif language == 'Japanese':
-        return domain + 'ja'
-    elif language == 'Spanish':
-        return domain + 'es'
-    elif language == 'Russian':
-        return domain + 'ru'
-    else:
-        domain
+def factorial(n: int) -> int:
+    result = 1
+    for i in range(n):
+        print(i)
+        result += i * result
+        print(result)
+
+    return result
+
+
+# 修正コード
+def factorial(n):
+    output = 1
+    for i in range(n):
+        output += i * output
+    return output
