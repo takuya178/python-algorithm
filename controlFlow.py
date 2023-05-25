04add31bb387d00cb80e69836346736a279c7984
takuya178 <takuya.u1005@gmail.com>
2023-05-25 09:31:50 +0900

    add 2の補数

--- controlFlow.py
+++ controlFlow.py
@@ -190,3 +190,30 @@ def oneComplement(bits: str) -> str:
         outPut += '0' if bits[i] == '1' else '1'
 
     return outPut
+
+def twosComplement(bits):
+    twosComplement = onesComplement(bits)
+    carryOut = False
+
+    for i in reversed(range(0, len(twosComplement))):
+        if twosComplement[i] == '0':
+            twosComplement = twosComplement[:i] + '1' + twosComplement[i + 1:]
+            carryOut = False
+            break
+
+        elif twosComplement[i] == '1':
+            twosComplement = twosComplement[:i] + '0' + twosComplement[i + 1:]
+            carryOut = True
+
+    return '1' + twosComplement if carryOut else twosComplement
+
+
+def onesComplement(bits):
+    onesComplement = ''
+    for bit in bits:
+        if bit == '1':
+            onesComplement += '0'
+        else:
+            onesComplement += '1'
+    return onesComplement
+
