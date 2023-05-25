811f81c7753ca888628300d6d1f2f9f2af45f1a1
takuya178 <takuya.u1005@gmail.com>
2023-05-26 08:35:39 +0900

    add dog

--- object.py
+++ object.py
@@ -20,3 +20,21 @@ print(carly.getInitial())
 jessie = Person("Jessie", "Raelynn")
 print(jessie.getFullName())
 print(jessie.getInitial())
+
+
+class Dog:
+    def __init__(self, name, size, age):
+        self.name = name
+        self.size = size
+        self.age = age
+
+    def bark(self) -> str:
+        if self.size >= 50:
+            return 'Wooof! Woof!'
+        elif self.size >= 20:
+            return 'Ruff! Ruff!'
+        else:
+            return 'Yip! Yip!'
+
+    def calcHumanAge(self) -> int:
+        return 12 + (self.age - 1) * 7
