03245a4e97fa7fd0e979edfea8d150d0ab589d66
takuya178 <takuya.u1005@gmail.com>
2023-06-01 21:59:54 +0900

    add sortByMax

--- array.py
+++ array.py
@@ -5,3 +5,19 @@ def getMaxIndex(arr: list):
         if arr[i] == max_num:
             results.append(i)
     return max(results)
+
+def sortByMaxMin(arr: int):
+    sortList = []
+    while len(arr) > 0:
+        maxNumber = max(arr)
+        minNumber = min(arr)
+        if len(arr) == 1:
+            sortList.append(maxNumber)
+        else:
+            sortList.append(maxNumber)
+            sortList.append(minNumber)
+        arr.remove(maxNumber)
+        if len(arr) <= 1: break
+        arr.remove(minNumber)
+
+    return sortList
