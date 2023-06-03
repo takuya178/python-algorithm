bb9c28267139324b9992609da801f0ec5066cc03
takuya178 <takuya.u1005@gmail.com>
2023-06-03 12:35:29 +0900

    add 文字コード

--- curdinal.py
+++ curdinal.py
@@ -120,3 +120,37 @@ def charInBagOfWordsCount(bagOfWords: list, keyCharacter: int):
                 count += 1
 
     return count
+import string
+
+def createAscObj():
+    obj = {}
+    ascNum = 97
+    for char in string.ascii_lowercase:
+        obj[char] = ascNum
+        ascNum += 1
+
+    return obj
+
+def isFirstStringLarger(s1: str, s2: str) -> bool:
+    sumS1Number = 0
+    sumS2Number = 0
+    s1 = s1.lower().replace(' ', '')
+    s2 = s2.lower().replace(' ', '')
+    for i in range(len(s1)):
+        sumS1Number += createAscObj()[s1[i]]
+    for i in range(len(s2)):
+        sumS2Number += createAscObj()[s2[i]]
+
+    return True if sumS1Number > sumS2Number else False
+
+
+
+def isFirstStringLarger(s1,s2):
+    return getAscii(s1) > getAscii(s2)
+
+def getAscii(string):
+    sumOfAscii = 0
+    for char in string.lower():
+        sumOfAscii += ord(char)
+    return sumOfAscii
+
