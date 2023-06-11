319a57830fa4ae380787eadd70cc58b24e25fd87
takuya178 <takuya.u1005@gmail.com>
2023-06-11 10:31:23 +0900

    add curdinal

--- curdinal.py
+++ curdinal.py
@@ -13,9 +13,7 @@ def websitePagination(urls: list, pageSize: int, page: int) -> list:
 
     return pagenationList[page-1]
 
-print(websitePagination(["url1","url2","url3","url4","url5","url6"],4,1) )
 
-print(9 % 3)
 
 def hasPenalty(records: list) -> bool:
     isPenalty = True
@@ -27,4 +25,35 @@ def hasPenalty(records: list) -> bool:
             break
         records = records[1:]
     return isPenalty
-print(hasPenalty([1,2,3,7]))
+
+def isMountain(height):
+    maxNumIndex = height.index(max(height))
+    isHeight = True
+
+    if maxNumIndex == 0 or maxNumIndex == len(height)-1: return False
+
+    for i in range(maxNumIndex):
+        if height[i] > height[i+1] or height[i] == height[i+1]:
+            isHeight = False
+            break
+
+    for i in range(maxNumIndex, len(height)-1):
+        if height[i] < height[i+1] or height[i] == height[i+1]:
+            isHeight = False
+            break
+
+    return isHeight
+
+print(isMountain([1,2,3,2])) # True
+print(isMountain([1,2]))
+print(isMountain([2,1]))
+print(isMountain([1,2,2,2,2]))
+print(isMountain([1,2,3]) )
+print(isMountain([4,3,2,1]))
+print(isMountain([1,2,2,2,3,2])) # 高さは初めは上がり続ける
+print(isMountain([3,2,2,2,1,1]))
+print(isMountain([10,20,30,400,500,10])) # True
+print(isMountain([100,200,100,400,500,100]))
+print(isMountain([100,200,300,200,100,300]))
+print(isMountain([100,50,100,200,300,400]))
+print(isMountain([53,158,477,994,994,867,797,755,744,621,616])) # 頂点の値が同じ
